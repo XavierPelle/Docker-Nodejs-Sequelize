25306cafd89cf163d555826eb9a1669297377dad
Xavier Pellé <114385560+XavierPelle@users.noreply.github.com>
2024-10-21 10:24:19 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+sequelize.sync()
+    .then(() => {
+        console.log('La base de données et la table sont synchronisées.');
+    })
+    .catch((error) => {
+        console.error('Erreur lors de la synchronisation de la base de données:', error);
+    });
+
+app.get("/", function (req, res) {
+    res.send("Base route");
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//psql -h db -U main -d jira_db
